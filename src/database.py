ac6ce1838a4287a059df87ef5e2965ec6b8e8741
Chris Archibald <carchi8py@gmail.com>
2017-12-24 13:22:53 -0800

    Add a many to many table for groups

--- src/database.py
+++ src/database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Date, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table
 
 Base = declarative_base()
+
+association_table = Table('association', Base.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('char_id', Integer, ForeignKey('character.id')),
+    Column('group_id', Integer, ForeignKey('group.id'))
+)
 
 class Character_DB(Base):
     __tablename__ = 'character'
@@ -11,6 +17,16 @@ class Character_DB(Base):
     name = Column(String(128), nullable=False)
     server_id = Column(Integer, ForeignKey('server.id'))
     server = relationship("Server_DB", back_populates="characters")
+    groups = relationship("Group_DB",
+                          secondary=association_table,
+                          back_populates="characters")
+
+class Group_DB(Base):
+    __tablename__ = "group"
+    id = Column(Integer, primary_key=True)
+    characters = relationship("Character_DB",
+                             secondary=association_table,
+                             back_populates="groups")
 
 class Server_DB(Base):
     __tablename__ = 'server'
@@ -18,9 +34,6 @@ class Server_DB(Base):
     name = Column(String(128), nullable=False)
     characters = relationship("Character_DB", back_populates="server")
 
-class Group_DB(Base):
-    __tablename__ = "group"
-    id = Column(Integer, primary_key=True)
 
 engine = create_engine('sqlite:///db.db')
 Base.metadata.create_all(engine)
